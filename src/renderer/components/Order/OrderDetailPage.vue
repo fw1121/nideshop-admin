a5f1bda22d25dbe8d62861eaf0438359cbe4a7fc
stevensl <592672849@qq.com>
2018-04-01 17:14:29 +0800

    增加订单详情页面功能

--- src/renderer/components/Order/OrderDetailPage.vue
+++ src/renderer/components/Order/OrderDetailPage.vue
@@ -11,7 +11,7 @@
         <div class="content-main">
             <div class="form-table-box" v-loading="pageLoading" element-loading-text="拼命加载中">
                
-               <el-form ref="infoForm" :rules="infoRules" :model="infoForm" label-width="120px">
+               <el-form ref="infoForm" :model="infoForm" label-width="120px">
 
                     <el-form-item prop="deletedPics">
                     </el-form-item>
@@ -20,8 +20,9 @@
                         {{infoForm.order_sn}}
                     </el-form-item>
                     <el-form-item label="订单状态" prop="order_status">
-                        {{infoForm.order_status}}
-                        <el-button type="primary" @click="onSubmitInfo">修改状态</el-button>
+                        {{infoForm.orderStatusText}}
+
+                        <el-button v-if="infoForm.order_status==0||infoForm.order_status==1||infoForm.order_status==2" type="primary" @click="modifyOrderStatus">{{ infoForm.order_status == 0 ? '已与买家确认' : infoForm.order_status == 1 ? '买家已付款' : infoForm.order_status == 2 ? '已发货' : '' }}</el-button>
                     </el-form-item>
 
                     <el-form-item label="订单价格" prop="order_price">
@@ -29,13 +30,14 @@
                     </el-form-item>
 
                     <el-form-item label="下单时间" prop="add_time">
-                        {{infoForm.add_time}}
+                        {{ infoForm.add_time | formatDate }}
                     </el-form-item>
                     
                     <el-form-item label="收货人" prop="consignee">
                         {{infoForm.consignee}}
                     </el-form-item>
                     <el-form-item label="收货地址">
+                        {{infoForm.detailAddress}}
                     </el-form-item>
 
                     <el-form-item label="联系方式" prop="mobile">
@@ -48,6 +50,13 @@
 
                     <el-form-item label="商品详情">
                         
+                        <div v-for="(item) in infoForm.goodsInfos" :key="item.list_pic_url">
+                            <div class="image-uploader"> 
+                                <img v-if="item.list_pic_url" :src="item.list_pic_url" class="image-show">
+                                {{item.name}}
+                                ¥{{item.retail_price}}
+                            </div>
+                        </div>
                     </el-form-item>
 
                     <el-form-item>
@@ -62,73 +71,156 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                pageLoading: false,
-                infoForm: {
-                    id: 0,
-                    order_sn: 0,
-                    order_status:0,
-                    order_price:0,
-                    add_time:0,
-                    consignee:'',
-                    mobile:'',
-                    postscript:'',
-                }
-            }
-        },
-        methods: {
-            goBackPage() {
-                this.$router.go(-1);
-            },
-            getInfo() {
-                if (this.infoForm.id <= 0) {
-                    return false
-                }
+import DateUtil from "@/js/DateUtil";
 
-                //加载订单详情
-                let that = this
-                this.axios.get('order/info', {
+export default {
+  data() {
+    return {
+      pageLoading: false,
+      infoForm: {
+        id: 0,
+        order_sn: 0,
+        order_status: 0,
+        orderStatusText:'',
+        order_price: 0,
+        add_time: 0,
+        consignee: "",
+        mobile: "",
+        postscript: "",
+        detailAddress: "",
+        goodsInfos:[],
+      }
+    };
+  },
+  methods: {
+    goBackPage() {
+      this.$router.go(-1);
+    },
+    getInfo() {
+      if (this.infoForm.id <= 0) {
+        return false;
+      }
+
+      //加载订单详情
+      let that = this;
+      this.axios
+        .get("order/info", {
+          params: {
+            id: that.infoForm.id
+          }
+        })
+        .then(response => {
+          console.log(response.data);
+          let resInfo = response.data.data;
+          that.infoForm = resInfo;
+          this.pageLoading = false;
+        });
+    },
+
+    /** 修改订单状态 */
+    modifyOrderStatus() {
+
+        let that = this;
+        this.$confirm('确定要执行此操作?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+            if(that.infoForm.order_status==0) //当前为等待确认状态，处理为已确认状态
+            {
+                that.infoForm.order_status=1;
+            } 
+            else if(that.infoForm.order_status==1) //当前为已确认，等待付款状态，处理为已付款，等待发货状态
+            {
+                that.infoForm.order_status=2;
+            }
+            else if(that.infoForm.order_status==2) //当前为已付款等待发货状态，处理为已发货状态
+            {
+                that.infoForm.order_status=3;
+            }
+
+            that.axios.get("order/orderStatusToText", {
                     params: {
-                        id: that.infoForm.id
+                        order_status: that.infoForm.order_status
                     }
-                }).then((response) => {
-                    console.log(response.data);
-                    let resInfo = response.data.data;
-                    that.infoForm = resInfo;
-                    this.pageLoading = false;
-                })
-            },
+                    })
+                    .then(response => {
+                        that.infoForm.orderStatusText = response.data.data;
+                    });
+        });
+    },
 
-            /** 修改订单状态 */
-            modifyOrderStatus()
-            {
-                
-            },
+    /** 关闭订单 */
+    closeOrder() {},
 
-            /** 关闭订单 */
-            closeOrder()
-            {
-                
-            },
-
-            onSubmitInfo()
-            {
-
+    onSubmitInfo() {
+        this.$refs["infoForm"].validate(valid => {
+        if (valid) {
+          this.axios.post("order/store", this.infoForm).then(response => {
+            if (response.data.errno === 0) {
+              this.$message({
+                type: "success",
+                message: "保存成功"
+              });
+              this.$router.go(-1);
+            } else {
+              this.$message({
+                type: "error",
+                message: "保存失败"
+              });
             }
-
-        },
-        components: {},
-        mounted() {
-            console.log(this.$route.query)
-            this.infoForm.id = this.$route.query.id || 0;
-            this.getInfo();
+          });
+        } else {
+          return false;
         }
+      });
     }
+  },
+  components: {},
+  mounted() {
+    console.log(this.$route.query);
+    this.infoForm.id = this.$route.query.id || 0;
+    this.getInfo();
+  },
 
+  filters: {
+    formatDate(time) {
+      var date = new Date(time * 1000);
+      return DateUtil.formatDate(date, "yyyy-MM-dd hh:mm:ss");
+    }
+  }
+};
 </script>
 
 <style>
-   
+.image-uploader {
+  height: 130px;
+  display: inline-block;
+}
+.image-uploader .el-upload {
+  border: 1px solid #d9d9d9;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.image-uploader .el-upload:hover {
+  border-color: #20a0ff;
+}
+
+.image-uploader .image-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 187px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+
+.image-uploader .image-show {
+  width: 187px;
+  height: 100px;
+  display: block;
+  float: left;
+}
 </style>
