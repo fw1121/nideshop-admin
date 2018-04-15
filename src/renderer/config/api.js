3785b3c2a4ab4853c55c17724db492488b6243a8
stevensl <592672849@qq.com>
2018-04-15 19:50:22 +0800

    修改配置为正式环境

--- src/renderer/config/api.js
+++ src/renderer/config/api.js
@@ -1,7 +1,7 @@
-const rootUrl = 'http://127.0.0.1:8360/admin/';
-//const rootUrl = 'http://flyinthesky.cn:8360/admin/';
+//const rootUrl = 'http://127.0.0.1:8360/admin/';
+const rootUrl = 'http://flyinthesky.cn:8360/admin/';
 const version = 'v0.0.4';
-const isShowDelete = true; // 是否显示删除按钮，上线后的版本不显示
+const isShowDelete = false; // 是否显示删除按钮，上线后的版本不显示
 const api = {
     rootUrl:rootUrl,
     version:version,
